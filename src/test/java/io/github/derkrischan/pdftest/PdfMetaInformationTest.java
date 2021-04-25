5adac764b857d065897eacccb203bdbbff35bdc4
Christian Wiech <derkrischan.bln@googlemail.com>
2021-04-25 16:47:05 +0200

    Remove unused imports

--- src/test/java/io/github/derkrischan/pdftest/PdfMetaInformationTest.java
+++ src/test/java/io/github/derkrischan/pdftest/PdfMetaInformationTest.java
@@ -2,11 +2,10 @@ package io.github.derkrischan.pdftest;
 
 import org.junit.Test;
 
-import io.github.derkrischan.pdftest.PdfAssertions;
-
 /**
  * 
- * Tests for several assertions that can be performed for meta informations stored in PDF.
+ * Tests for several assertions that can be performed for meta informations
+ * stored in PDF.
  * 
  * @author krischan
  *
@@ -16,43 +15,43 @@ public class PdfMetaInformationTest {
 	@Test
 	public void testVerifyAuthor() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.author().isNotBlank().matches("Evangelos Vlachogiannis");
+		    .author().isNotBlank().matches("Evangelos Vlachogiannis");
 	}
 
 	@Test
 	public void testVerifyCreator() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.creator().matches("Writer");
+		    .creator().matches("Writer");
 	}
 
 	@Test
 	public void testVerifySubject() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.subject().isBlank();
+		    .subject().isBlank();
 	}
 
 	@Test
 	public void testVerifyTitle() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.title().isNull();
+		    .title().isNull();
 	}
 
 	@Test
 	public void testVerifyProducer() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.producer().startsWith("OpenOffice.org");
+		    .producer().startsWith("OpenOffice.org");
 	}
 
 	@Test
 	public void testVerifyCreationDate() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.creationDate().isBeforeOrEqualsTo("2007-02-24")
-		.isAfter("2007-02-22");
+		    .creationDate().isBeforeOrEqualsTo("2007-02-24")
+		    .isAfter("2007-02-22");
 	}
 
 	@Test
 	public void testVerifyVersion() {
 		PdfAssertions.assertThat(ClassLoader.getSystemResourceAsStream("pdf/dummy.pdf"))
-		.version().isEqualTo(1.4f);
+		    .version().isEqualTo(1.4f);
 	}
 }
